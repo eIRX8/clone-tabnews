7c436485cbb4afdf34258ef22da9ee2536d23e21
Ismael Rodrigues <irx800@gmail.com>
2025-04-22 14:23:21 +0000

    Frase Modificada V3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home(){
     return 
-    <>Ola! Visitante.
+    <>
+    <br>Ola! Visitante.</br>
     <br>Imite um Lobo.</br>
     <br>e SORRIA!!!</br>
     </>
 }
 
 export default Home;
+
+
